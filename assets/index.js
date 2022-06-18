b1ce3e47565443255063db253cc5f3f128be0401
Alejandro Castaneda <alejandroxd62@gmail.com>
2022-06-18 18:47:43 -0500

    intersection observer. 21

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,7 @@
 import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './plugins/AutoPlay.js';
+import AutoPause from './plugins/AutoPause.js';
+
 const video = document.querySelector('video');
 const button = document.querySelector('button');
 
